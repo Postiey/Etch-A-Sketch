d130cd96575641bfac4162fc3cb2484344616e9e
Tyler Postuma <posty.tyler@gmail.com>
2022-05-01 23:26:43 -0500

    Created greyscale functionality and am working on adding the ability to use the toggle buttons at the bottom of the screen.

--- main_script.js
+++ main_script.js
@@ -4,24 +4,56 @@
 // Create functions to change the width and the height of the playboard
 // Create a function to allow the user to change colors to any color
 // Create a function for a random color
+const buttons = document.querySelectorAll('.btn');
+
+function buttonListener(value) {
+    buttons.forEach(btn => {
+        btn.addEventListener('click', e => {
+            if (btn.innerText === 'Rainbow') {
+                let newValue = value;
+                newValue.style.backgroundColor = `rgb(${randRainColor()}, ${randRainColor()}, ${randRainColor()})`;
+            }
+        });
+    });
+}
+
+buttonListener();
+
 
 function createBoard(size) {
     const board = document.querySelector('.gridContainer');
     board.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
     board.style.gridTemplateRows = `repeat(${size}, 1fr)`;
 
-
-
     let gridSize = size*size;
 
     for (let i = 0; i <= gridSize; i++) {
-        let square = document.createElement('div');
-        square.style.backgroundColor = 'blue';
+        const square = document.createElement('div');
+        square.style.backgroundColor = 'lightgray';
         board.appendChild(square);
-        square.addEventListener('mouseenter', e => {
-            square.style.backgroundColor = 'black';
-        });
+
+        buttonListener('square');
+
     }
+};
+
+// Creates a random rainbow color when the user drags their mouse over boxes.
+function randRainColor() {
+    let randNum = Math.floor(Math.random() * 255);
+    return randNum;
 }
 
-createBoard(4);
+// Creates a greyscale pattern with an array of colors
+function randomGreyScale() {
+    let greyCodes = ['525252', '7A7A7A', 'D6D6D6', '000', '292929'];
+    let randNum = Math.floor(Math.random() * 5);
+    let newNumber = greyCodes[randNum];
+    let colorCode = "#" + newNumber;
+    return colorCode;  
+}
+
+
+
+
+
+createBoard(10);
